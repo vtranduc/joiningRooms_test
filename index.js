adc4b3e72058b1c4cc6875fdfbea42e17f07c7db
Viet Nhien Tran Duc <vtranduc@ryerson.ca>
2019-07-30 18:15:33 +0000

    updated

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ var socket = require('socket.io');
 // App setup
 
 var app = express();
-var server = app.listen(8080, () => {
-  console.log('Port ready on 8080');
-});
+
 
 // Static files
 
@@ -14,45 +12,93 @@ app.use('/public', express.static('./public'));
 
 app.set("view engine", "ejs");
 
+var server = app.listen(8080, () => {
+  console.log('Port ready on 8080');
+});
+
+// Initializing the browser
+
+app.get("/", (req, res) => {
+  res.render("lobby", {game_data
+  })
+})
 
 // // Socket setup
 
 var io = socket(server);
 
-room_data = {
-  rabbit: {},
-  dog: {},
-  Woodpecker: {}
-};
 game_data ={
-  whosbigger:{room_data: {}},
-  kingsCup: {room_data: {}},
+  whosbigger:{
+    room_data: {
+      woodpecker: {},
+      dragon: {},
+      hippo: {}
+    }
+  },
+  kingsCup: {room_data: {woofpecker: {}}},
   goofy: {room_data: {}}
 }
-
-app.get("/", (req, res) => {
-  res.render("lobby", {room_data, game_data
-  })
-})
-
 
 io.on('connection', (socket) => {
 
   console.log(socket.id)
 
-  socket.on('creatingRoom', (data) => {
-    if (room_data[data.roomName]) {
-    } else {
-      room_data[data.roomName] = {};
-      io.sockets.emit('creatingRoom', data)
+  // socket.on('creatingRoom', (data) => {
+  //   if (room_data[data.roomName]) {
+  //   } else {
+  //     room_data[data.roomName] = {};
+  //     io.sockets.emit('creatingRoom', data)
+  //   }
+  // });
+
+
+  // socket.on('joiningRoom', (data) => {
+  //   socket.join(data);
+  //   var clients = io.sockets.adapter.rooms[data].sockets;
+  //   io.sockets.to(data).emit('addingNewUser', clients);
+  // });
+
+
+  socket.on('joinARoom', (data) => {
+
+    // Leave all the rooms
+
+    for (joinedRoom of getJoinedRooms(game_data, io, socket.id)) {
+      socket.leave(joinedRoom);
+      let room_info = io.sockets.adapter.rooms[joinedRoom];
+      if (room_info) {
+        io.sockets.to(joinedRoom).emit('updateUserList', room_info.sockets);
+      }
     }
+
+    // Join the room
+
+    const uniqueRoomName = `${data.gameId}-${data.roomId}`;
+    socket.join(uniqueRoomName)
+    var clients = io.sockets.adapter.rooms[uniqueRoomName].sockets;
+    io.sockets.to(uniqueRoomName).emit('updateUserList', clients);
   });
 
-
-  socket.on('joiningRoom', (data) => {
-    socket.join(data);
-    var clients = io.sockets.adapter.rooms[data].sockets;
-    io.sockets.to(data).emit('addingNewUser', clients);
-  });
 
 });
+
+
+//=============== HELPER FUNCTIONS =================================================
+
+// This function obtains all the rooms the user is joined.
+// Note that it may return more than 1 room
+
+const getJoinedRooms = function(game_data, io, userID) {
+  output = [];
+  for (let gameName in game_data) {
+    for (let roomName in game_data[gameName].room_data) {
+      let room_data = io.sockets.adapter.rooms[`${gameName}-${roomName}`];
+      if (room_data) {
+        if (room_data.sockets[userID]) {
+          output.push(`${gameName}-${roomName}`);
+        }
+      }
+    }
+  }
+  return output;
+};
--- public/roomCreate.js
+++ public/roomCreate.js
@@ -4,7 +4,8 @@ console.log('hello world')
 
 const createBtn = document.getElementById('createBtn');
 const assignRoomName = document.getElementById('assignRoomName');
-const roomList = document.getElementById('roomList')
+// const roomList = document.getElementById('roomList')
+const roomList = document.getElementsByClassName('roomList')
 
 createBtn.addEventListener('click', () => {
   socket.emit('creatingRoom', {
@@ -12,34 +13,63 @@ createBtn.addEventListener('click', () => {
   });
 });
 
+//====================================================
 
-
-let createdRooms = document.getElementsByClassName('createdRooms');
-
-for (let room of createdRooms) {
-  room.addEventListener("click", () => {
-    socket.emit('joiningRoom', room.value)
+const gameList = document.getElementsByClassName('game');
+for (let game of gameList) {
+  // console.log(game)
+  const gameShower = document.getElementById(game.value)
+  game.addEventListener('click', (event) => {
+    for (let gameBar of roomList) {
+      gameBar.style.display = "none";
+    }
+    gameShower.style.display = "block";
   })
 }
 
-socket.on('creatingRoom', (data) => {
-  console.log('2')
-
-  const btn = document.createElement('button');
-  btn.addEventListener('click', () => {
-    console.log('Hello hi')
-    socket.emit('joiningRoom', data.roomName)
-  })
-  btn.innerHTML = data.roomName;
-  roomList.appendChild(btn);
-
+$(document).ready(() => {
+  
+  $('.room').on('click', function () {
+    const roomId = $(this).attr('data-roomid');
+    const gameId = $(this).attr('data-gameid')
+    socket.emit('joinARoom', {roomId: roomId, gameId: gameId})
+  });
 });
 
-const logs = document.getElementById('logs');
-
-socket.on('addingNewUser', (data) => {
-  logs.innerHTML ="";
+socket.on('updateUserList', (data) => {
+  logs.innerHTML = "";
   for (let key in data) {
     logs.innerHTML += `<p>${key}</p>`
-  }
+  };
 });
+
+//====================================================
+
+// let createdRooms = document.getElementsByClassName('createdRooms');
+
+// for (let room of createdRooms) {
+//   room.addEventListener("click", () => {
+//     socket.emit('joiningRoom', room.value)
+//   })
+// }
+
+// socket.on('creatingRoom', (data) => {
+//   console.log('2')
+
+//   const btn = document.createElement('button');
+//   btn.addEventListener('click', () => {
+//     console.log('Hello hi')
+//     socket.emit('joiningRoom', data.roomName)
+//   })
+//   btn.innerHTML = data.roomName;
+//   roomList.appendChild(btn);
+// });
+
+// const logs = document.getElementById('logs');
+
+// socket.on('addingNewUser', (data) => {
+//   logs.innerHTML ="";
+//   for (let key in data) {
+//     logs.innerHTML += `<p>${key}</p>`
+//   }
+// });
